114298529e49e9a47c6bd15e3d97d2c91cd6afcd
gtshekel <gtshekel@student.42.fr>
2017-12-03 05:46:47 +0200

    prepare to read in map

--- exprt/src/expert/main.java
+++ exprt/src/expert/main.java
@@ -1,12 +1,45 @@
 package expert;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class main {
 
-    public  static  void main (String args[])
-    {
-        System.out.println("whpopovhvjvjhvjhvoops");
-        Fact fact = new Fact('A');
-        boolean  temp = fact.is_determined();
-        System.out.println(fact.get_value());
+    public  static  void main (String args[]) {
+
+
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try
+        {
+            fr = new FileReader(args[1]);
+            br = new BufferedReader(fr);
+
+            String Line;
+
+            while ((Line = br.readLine()) != null)
+            {
+                System.out.println(Line);
+            }
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
     }
 }
